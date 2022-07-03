7e9fe16b2fe1d0a37d24a694ffe57cb343c476a4
FarhanaAfrin <farhanaafrineirin@gmail.com>
2022-07-03 12:37:50 +0600

    fix

--- zappit_project_section2/zappit/settings.py
+++ zappit_project_section2/zappit/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4#j5knl_mslj-p233=gb@h^tvj#r$j7%#i&^@@uu2s2=-p5v2q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
